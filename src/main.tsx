4eaf1c8e9365fe7c0f95ff937f0b0e80dfe08895
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-19 09:08:26 +0700

    Remove default + add custom css + typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Calculator from './Calculator'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementsByTagName('main')[0] as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>,
 )
