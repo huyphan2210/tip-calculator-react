dfeba5c87faa415ed91beeaf2fa9b32264be7805
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-20 09:09:35 +0700

    Handle states

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { MouseEvent, useState } from 'react';
 import './Calculator.css';
 
 import CustomInput from './components/CustomInput';
@@ -9,6 +9,22 @@ import iconPerson from './assets/images/icon-person.svg';
 function Calculator() {
   const billLabel = 'Bill';
   const numOfPeopleLabel = 'Number of People';
+
+  const [bill, setBill] = useState(0);
+  const [numOfPeople, setNumOfPeople] = useState(0);
+
+  const [tipAmount, setTipAmount] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const [isResetDisable, setIsResetDisable] = useState(true);
+
+  const handleSelectTip = (e: MouseEvent) => {
+    document.querySelectorAll('#tipPercentage button').forEach((button) => {
+      button.setAttribute('style', '');
+    })
+    e.currentTarget.setAttribute('style', 'color: var(--very-dark-cyan); background-color: var(--strong-cyan);')
+  }
+
   return (
     <div className="calculator">
       <div className='inputs'>
@@ -16,24 +32,35 @@ function Calculator() {
         <div className='selectTip'>
           <label htmlFor='tipPercentage'><small>Select Tip %</small></label>
           <div id="tipPercentage">
-            <button type='button'>5%</button>
-            <button type='button'>10%</button>
-            <button type='button'>15%</button>
-            <button type='button'>25%</button>
-            <button type='button'>50%</button>
-            <input placeholder='Custom'></input>
+            <button type='button' onClick={(e) => handleSelectTip(e)}>5%</button>
+            <button type='button' onClick={(e) => handleSelectTip(e)}>10%</button>
+            <button type='button' onClick={(e) => handleSelectTip(e)}>15%</button>
+            <button type='button' onClick={(e) => handleSelectTip(e)}>25%</button>
+            <button type='button' onClick={(e) => handleSelectTip(e)}>50%</button>
+            <div>
+              <img src={iconDollar} alt='Custom Tip'></img>
+              <input type='text' placeholder='Custom'></input>
+            </div>
           </div>
         </div>
         <CustomInput label={numOfPeopleLabel} icon={iconPerson}></CustomInput>
       </div>
       <div className='result'>
-        Tip Amount
-        / person
-
-        Total
-        / person
-
-        Reset
+        <div className='calc'>
+          <div className='calcLabel'>
+            <span>Tip Amount</span>
+            <small>/ person</small>
+          </div>
+          <h1>${tipAmount}</h1>
+        </div>
+        <div className='calc'>
+          <div className='calcLabel'>
+            <span>Total</span>
+            <small>/ person</small>
+          </div>
+          <h1>${total}</h1>
+        </div>
+        <button type='button' disabled={isResetDisable}>Reset</button>
       </div>
     </div>
   )
