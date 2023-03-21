d65ad464623e3ca8b78e31920d4cde51c0f303ff
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-21 09:13:59 +0700

    Add handlers

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useState } from 'react';
+import { MouseEvent, KeyboardEvent, useState, ChangeEvent } from 'react';
 import './Calculator.css';
 
 import CustomInput from './components/CustomInput';
@@ -13,8 +13,9 @@ function Calculator() {
   const [bill, setBill] = useState(0);
   const [numOfPeople, setNumOfPeople] = useState(0);
 
+  const [customTip, setCustomTip] = useState(0)
+
   const [tipAmount, setTipAmount] = useState(0);
-  const [total, setTotal] = useState(0);
 
   const [isResetDisable, setIsResetDisable] = useState(true);
 
@@ -23,12 +24,77 @@ function Calculator() {
       button.setAttribute('style', '');
     })
     e.currentTarget.setAttribute('style', 'color: var(--very-dark-cyan); background-color: var(--strong-cyan);')
+    setTipAmount(parseInt(e.currentTarget.innerHTML.replace('%', '')) / 100);
+    setCustomTip(0)
+  }
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (!preventInvalidLetters(e, /^[0-9.]+$/)) e.preventDefault();
+  }
+
+  const handleChangeBill = (e: ChangeEvent<HTMLInputElement>) => {
+    const billValue = parseFloat(e.currentTarget.value);
+    if (billValue === 0 || isNaN(billValue) || e.currentTarget.value === '') {
+      document.getElementsByClassName('input')[0].getElementsByTagName('small')[1].setAttribute('style', 'display: block;');
+      e.currentTarget.setAttribute('class', 'error');
+      return false;
+    } else {
+      document.getElementsByClassName('input')[0].getElementsByTagName('small')[1].setAttribute('style', '');
+      e.currentTarget.setAttribute('class', '');
+    }
+    setBill(parseFloat(e.currentTarget.value));
+    if (numOfPeople) setIsResetDisable(false)
+    else setIsResetDisable(true)
+  }
+
+  const handleChangePeople = (e: ChangeEvent<HTMLInputElement>) => {
+    const people = parseFloat(e.currentTarget.value);
+    if (people === 0 || e.currentTarget.value === '') {
+      document.getElementsByClassName('input')[1].getElementsByTagName('small')[1].setAttribute('style', 'display: block;');
+      e.currentTarget.setAttribute('class', 'error');
+      return false;
+    } else {
+      document.getElementsByClassName('input')[1].getElementsByTagName('small')[1].setAttribute('style', '');
+      e.currentTarget.setAttribute('class', '');
+    }
+    setNumOfPeople(parseFloat(e.currentTarget.value));
+    if (bill) setIsResetDisable(false)
+    else setIsResetDisable(true)
+  }
+
+  const reset = () => {
+    for (let i = 0; i < document.getElementsByTagName('input').length; i++) {
+      document.getElementsByTagName('input')[i].value = ''; 
+      document.getElementsByTagName('input')[i].setAttribute('class', '');
+    }
+    setBill(0);
+    setNumOfPeople(0);
+    setIsResetDisable(true);
+
+    document.getElementsByClassName('input')[0].getElementsByTagName('small')[1].setAttribute('style', '');
+    document.getElementsByClassName('input')[1].getElementsByTagName('small')[1].setAttribute('style', '');
+
+    document.querySelectorAll('#tipPercentage button').forEach((button) => {
+      button.setAttribute('style', '');
+    })
+  }
+
+  const hanldeCustomTip = (e: ChangeEvent<HTMLInputElement>) => {
+    setTipAmount(0);
+    setCustomTip(isNaN(parseFloat(e.currentTarget.value)) ? 0 : parseFloat(e.currentTarget.value));
+    document.querySelectorAll('#tipPercentage button').forEach((button) => {
+      button.setAttribute('style', '');
+    })
+  }
+
+  function preventInvalidLetters(e: KeyboardEvent, regexr: RegExp) {
+    return regexr.test(e.key) || e.key === 'Tab' || e.key === 'Backspace' || e.key === 'Delete' || e.key === 'F5' || e.key === 'ArrowRight' || e.key === 'ArrowLeft';
   }
 
   return (
     <div className="calculator">
       <div className='inputs'>
-        <CustomInput label={billLabel} icon={iconDollar}></CustomInput>
+        <CustomInput handleChange={handleChangeBill} label={billLabel} icon={iconDollar}></CustomInput>
         <div className='selectTip'>
           <label htmlFor='tipPercentage'><small>Select Tip %</small></label>
           <div id="tipPercentage">
@@ -39,11 +105,11 @@ function Calculator() {
             <button type='button' onClick={(e) => handleSelectTip(e)}>50%</button>
             <div>
               <img src={iconDollar} alt='Custom Tip'></img>
-              <input type='text' placeholder='Custom'></input>
+              <input maxLength={5} onChange={hanldeCustomTip} type='text' placeholder='Custom' onKeyDown={(e) => handleKeyDown(e)}></input>
             </div>
           </div>
         </div>
-        <CustomInput label={numOfPeopleLabel} icon={iconPerson}></CustomInput>
+        <CustomInput handleChange={handleChangePeople} label={numOfPeopleLabel} icon={iconPerson}></CustomInput>
       </div>
       <div className='result'>
         <div className='calc'>
@@ -51,16 +117,16 @@ function Calculator() {
             <span>Tip Amount</span>
             <small>/ person</small>
           </div>
-          <h1>${tipAmount}</h1>
+          <h1>${Math.round((customTip && numOfPeople ? customTip / numOfPeople : tipAmount && numOfPeople ? bill * tipAmount / numOfPeople : 0) * 100) / 100}</h1>
         </div>
         <div className='calc'>
           <div className='calcLabel'>
             <span>Total</span>
             <small>/ person</small>
           </div>
-          <h1>${total}</h1>
+          <h1>${Math.round((customTip && numOfPeople ? (bill + customTip) / numOfPeople : tipAmount && numOfPeople ? (bill + bill * tipAmount) / numOfPeople : numOfPeople ? bill / numOfPeople : 0) * 100) / 100}</h1>
         </div>
-        <button type='button' disabled={isResetDisable}>Reset</button>
+        <button onClick={reset} type='button' disabled={isResetDisable}>Reset</button>
       </div>
     </div>
   )
